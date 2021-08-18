c493a01a3459d2ab4e44c5ac643e9d9854d247d3
ducquang92 <ducquang92@users.noreply.github.com>
2021-08-18 12:44:57 +0200

    Update S3EnvironmentCheck.py

--- S3EnvironmentCheck.py
+++ S3EnvironmentCheck.py
@@ -7,6 +7,7 @@ class S3EnvironmentCheck(BaseResourceCheck):
         name = "Ensure s3 has environment tag of developemnt/staging/production"
         id = "CUSTOM_AWS_1"
         supported_resources = ['aws_s3_bucket']
+        print("Hello world")
         categories = [CheckCategories.GENERAL_SECURITY]
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
@@ -14,8 +15,10 @@ class S3EnvironmentCheck(BaseResourceCheck):
         if conf.get("tags",[]):
             env = conf["tags"][0].get("Environment",{})
             if env in ["Developemnt","Staging","Production"]:
+                #return CheckResult.PASSED
                 return CheckResult.PASSED
-        return CheckResult.FAILED
+        #return CheckResult.FAILED
+        return CheckResult.ABC
 
-
+print("Hello world")
 scanner = S3EnvironmentCheck()
